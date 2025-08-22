11491d3b1ea905789f4a6b7d58ce180eee6b3730
Ronald-exe <esteban1234volt@gmail.com>
2025-08-22 17:54:39 -0600

    firs_take

---
+++ code_asm/data_visualizer.asm
@@ -0,0 +1,15 @@
+section .data                                                           ; practice example
+    cons_hi: db 'Hola',0xa
+    hi_tamano: equ $-cons_hi
+
+section .text                                                           ;Section so that my breakpoints
+    global _start               
+
+_start:
+    mov rax,1
+    mov rdi,1
+    mov rsi,cons_hi
+    mov rdx,hi_tamano
+    syscall
+
+    
